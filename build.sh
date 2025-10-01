6a3f7e79e3e35bcdc2c78acfa2039cae6c9a2f10
DakshanyaDhanasekaran <dakshanyadhanasekaran2@gmail.com>
2025-10-01 15:17:14 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+# 1. Install all dependencies (Flask, Gunicorn)
 pip install -r requirements.txt
+
+# 2. Start the application using the installed Python module path
+#    The 'exec' ensures the script is replaced by the Gunicorn process.
+exec python -m gunicorn app:app
